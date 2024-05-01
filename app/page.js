402adfa1e978ed3c78e0693894801fea5883eec4
Franco Ayala <franco.a98@outlook.com>
2024-04-30 21:04:26 -0300

    Some changes

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
 import GameCard from "./gameCard"
+import ServerList from "./serversList";
 
 export default function Home() {
   return (
-    <div class="album py-5 bg-body-tertiary">
-      <div class="container">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <GameCard image="minecraft.webp"></GameCard>
-          <GameCard image="terraria.jpg"></GameCard>
-          <GameCard image="cs.jpg"></GameCard>
+    <>
+      <div className="album py-5 bg-body-tertiary">
+        <div className="container">
+          {/*<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">*/}
+          <div className="row">
+            <GameCard image="minecraft1.jpg"></GameCard>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
