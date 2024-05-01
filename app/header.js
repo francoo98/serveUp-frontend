f532b7fb2640ea8b5ab426bca62c50be9047e0fe
Franco Ayala <franco.a98@outlook.com>
2024-04-30 22:03:01 -0300

    Changed the header

--- app/header.js
+++ app/header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="py-3 mb-4 border-bottom">
+        <header className="py-3">
             <div className="container d-flex flex-wrap justify-content-center">
                 <a href="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
                     <span className="fs-4">ServeUp!</span>
