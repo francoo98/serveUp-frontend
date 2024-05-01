2aa3567080bdffc3c3726315042c99445a6655ad
Franco Ayala <franco.a98@outlook.com>
2024-04-30 21:03:22 -0300

    Made the image in GameCard responsive

--- app/gameCard.js
+++ app/gameCard.js
@@ -5,10 +5,11 @@ export default function GameCard({ image }) {
         <div className="col">
             <div className="card shadow-sm">
                 <div className='d-flex justify-content-center align-items-center'>
-                    <Image src={`/`+image} alt="minecraft" width="400" height="425" />
+                    {/*<Image src={`/`+image} alt="minecraft" width="1920" height="1080"/>*/}
+                    <img src={`/`+image} className="img-fluid rounded" alt={image}></img>
                 </div>
                 <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center">
+                    <div className="d-flex justify-content-center align-items-center">
                         <button type="button" className="btn btn-primary btn-sm">Create game server</button>
                     </div>
                 </div>
